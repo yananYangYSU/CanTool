72dd2c471559cb9e01219916862f66f00bcbda9f
huhongzhou <18712761936@163.com>
2017-10-13 16:03:14 +0800

    修改数据库并导入数据

--- cantool.sql
+++ cantool.sql
@@ -1,22 +1,22 @@
 /*
 Navicat MySQL Data Transfer
 
-Source Server         : localhost
-Source Server Version : 50541
+Source Server         : localhost_3306
+Source Server Version : 50719
 Source Host           : localhost:3306
 Source Database       : cantool
 
 Target Server Type    : MYSQL
-Target Server Version : 50541
+Target Server Version : 50719
 File Encoding         : 65001
 
-Date: 2017-10-13 11:05:10
+Date: 2017-10-13 15:54:24
 */
 
 SET FOREIGN_KEY_CHECKS=0;
 
 -- ----------------------------
--- Table structure for `candata`
+-- Table structure for candata
 -- ----------------------------
 DROP TABLE IF EXISTS `candata`;
 CREATE TABLE `candata` (
@@ -33,7 +33,7 @@ CREATE TABLE `candata` (
 -- ----------------------------
 
 -- ----------------------------
--- Table structure for `canmessage`
+-- Table structure for canmessage
 -- ----------------------------
 DROP TABLE IF EXISTS `canmessage`;
 CREATE TABLE `canmessage` (
@@ -47,9 +47,22 @@ CREATE TABLE `canmessage` (
 -- ----------------------------
 -- Records of canmessage
 -- ----------------------------
+INSERT INTO `canmessage` VALUES ('61', 'CDU_4', '8', 'CDU');
+INSERT INTO `canmessage` VALUES ('792', 'BCM_BCAN_1', '8', 'BCM');
+INSERT INTO `canmessage` VALUES ('797', 'HVAC_3', '8', 'HVAC');
+INSERT INTO `canmessage` VALUES ('800', 'HVAC_1', '8', 'HVAC');
+INSERT INTO `canmessage` VALUES ('801', 'HVAC_2', '8', 'HVAC');
+INSERT INTO `canmessage` VALUES ('837', 'BCM_ESC_2', '8', 'BCM');
+INSERT INTO `canmessage` VALUES ('856', 'CDU_1', '8', 'CDU');
+INSERT INTO `canmessage` VALUES ('864', 'HVAC_4', '8', 'ACP');
+INSERT INTO `canmessage` VALUES ('867', 'ACP_1', '8', 'ACP');
+INSERT INTO `canmessage` VALUES ('868', 'PTC_1', '8', 'PTC');
+INSERT INTO `canmessage` VALUES ('915', 'BCM_VCU_2', '8', 'BCM');
+INSERT INTO `canmessage` VALUES ('1056', 'BCM_NM', '8', 'BCM');
+INSERT INTO `canmessage` VALUES ('1067', 'CDU_NM', '8', 'CDU');
 
 -- ----------------------------
--- Table structure for `canmessagestr`
+-- Table structure for canmessagestr
 -- ----------------------------
 DROP TABLE IF EXISTS `canmessagestr`;
 CREATE TABLE `canmessagestr` (
@@ -73,7 +86,7 @@ CREATE TABLE `canmessagestr` (
 -- ----------------------------
 
 -- ----------------------------
--- Table structure for `cansignal`
+-- Table structure for cansignal
 -- ----------------------------
 DROP TABLE IF EXISTS `cansignal`;
 CREATE TABLE `cansignal` (
@@ -83,12 +96,12 @@ CREATE TABLE `cansignal` (
   `bitLength` tinyint(2) NOT NULL COMMENT 'bit段长 0-64',
   `bitType` tinyint(1) NOT NULL COMMENT '类型 0或1',
   `resolution` double(4,0) NOT NULL COMMENT 'A的值',
-  `offset` double(4,0) NOT NULL COMMENT 'B的值',
+  `offsets` double(4,0) NOT NULL COMMENT 'B的值',
   `minValue` double(4,0) NOT NULL COMMENT '物理最小值',
-  `maxValue` double(4,0) NOT NULL COMMENT '物理最大值',
+  `maxValues` double(4,0) NOT NULL COMMENT '物理最大值',
   `unit` varchar(12) DEFAULT NULL COMMENT '单位符号',
   `nodeNames` varchar(255) DEFAULT NULL COMMENT '要发送的节点字符串,逗号分隔',
-  PRIMARY KEY (`signalName`),
+  PRIMARY KEY (`messageId`,`signalName`),'因信号名称有重复，所以将messageId和signalName联合作为主键'
   KEY `can信息id` (`messageId`),
   CONSTRAINT `can信息id` FOREIGN KEY (`messageId`) REFERENCES `canmessage` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -96,3 +109,105 @@ CREATE TABLE `cansignal` (
 -- ----------------------------
 -- Records of cansignal
 -- ----------------------------
+INSERT INTO `cansignal` VALUES ('61', 'CDU_HVACACCfg', '1', '2', '0', '1', '0', '0', '3', '', 'HVAC');
+INSERT INTO `cansignal` VALUES ('61', 'CDU_HVACAirCirCfg', '3', '2', '0', '1', '0', '0', '3', '', 'HVAC');
+INSERT INTO `cansignal` VALUES ('61', 'CDU_HVACComfortCfg', '5', '2', '0', '1', '0', '0', '3', '', 'HVAC');
+INSERT INTO `cansignal` VALUES ('792', 'BCM_KeySt', '1', '2', '0', '1', '0', '1', '3', '', 'PEPS,ICM,AVM,CDU,HVAC');
+INSERT INTO `cansignal` VALUES ('797', 'HVAC_ACCfgSt', '0', '1', '0', '1', '0', '0', '1', '', 'CDU');
+INSERT INTO `cansignal` VALUES ('797', 'HVAC_AirCirCfgSt', '1', '1', '0', '1', '0', '0', '1', '', 'CDU');
+INSERT INTO `cansignal` VALUES ('797', 'HVAC_ComfortCfgSt', '3', '2', '0', '1', '0', '0', '1', '', 'CDU');
+INSERT INTO `cansignal` VALUES ('800', 'HVAC_ACmaxSt', '7', '1', '0', '1', '0', '0', '1', '', 'CDU');
+INSERT INTO `cansignal` VALUES ('800', 'HVAC_ACSt', '6', '1', '0', '1', '0', '0', '1', '', 'CDU');
+INSERT INTO `cansignal` VALUES ('800', 'HVAC_AirCirculationSt', '46', '2', '0', '1', '0', '0', '3', '', 'CDU');
+INSERT INTO `cansignal` VALUES ('800', 'HVAC_AirCompressorSt', '2', '3', '0', '1', '0', '0', '1', '', 'CDU');
+INSERT INTO `cansignal` VALUES ('800', 'HVAC_AutoSt', '30', '1', '0', '1', '0', '0', '1', '', 'CDU');
+INSERT INTO `cansignal` VALUES ('800', 'HVAC_CorrectedExterTemp', '15', '8', '0', '0', '-40', '-40', '87', '??C', 'BCM,CDU');
+INSERT INTO `cansignal` VALUES ('800', 'HVAC_CorrectedExterTempVD', '3', '1', '0', '1', '0', '0', '1', '', 'BCM,CDU');
+INSERT INTO `cansignal` VALUES ('800', 'HVAC_DriverTempSelect', '53', '5', '0', '0', '18', '18', '32', '??C', 'CDU');
+INSERT INTO `cansignal` VALUES ('800', 'HVAC_DualSt', '29', '1', '0', '1', '0', '0', '1', '', 'CDU');
+INSERT INTO `cansignal` VALUES ('800', 'HVAC_EngIdleStopProhibitReq', '5', '1', '0', '1', '0', '0', '1', '', 'CDU');
+INSERT INTO `cansignal` VALUES ('800', 'HVAC_IonMode', '55', '2', '0', '1', '0', '0', '3', '', 'CDU');
+INSERT INTO `cansignal` VALUES ('800', 'HVAC_PopUpDisplayReq', '47', '1', '0', '1', '0', '0', '1', '', 'CDU');
+INSERT INTO `cansignal` VALUES ('800', 'HVAC_PsnTempSelect', '48', '5', '0', '0', '18', '18', '32', '', 'CDU');
+INSERT INTO `cansignal` VALUES ('800', 'HVAC_RawExterTemp', '23', '8', '0', '0', '-40', '-40', '87', '??C', 'CDU');
+INSERT INTO `cansignal` VALUES ('800', 'HVAC_RawExterTempVD', '4', '1', '0', '1', '0', '0', '1', '', 'CDU');
+INSERT INTO `cansignal` VALUES ('800', 'HVAC_SpdFanReq', '36', '2', '0', '1', '0', '0', '1', '', 'CDU');
+INSERT INTO `cansignal` VALUES ('800', 'HVAC_TelematicsSt', '42', '3', '0', '1', '0', '0', '7', '', 'CDU');
+INSERT INTO `cansignal` VALUES ('800', 'HVAC_TempSelect', '28', '5', '0', '0', '18', '18', '32', '??C', 'CDU');
+INSERT INTO `cansignal` VALUES ('800', 'HVAC_Type', '31', '1', '0', '1', '0', '0', '1', '', 'CDU');
+INSERT INTO `cansignal` VALUES ('800', 'HVAC_WindExitMode', '34', '3', '0', '1', '0', '0', '7', '', 'CDU');
+INSERT INTO `cansignal` VALUES ('800', 'HVAC_WindExitSpd', '59', '4', '0', '1', '0', '0', '15', '', 'CDU');
+INSERT INTO `cansignal` VALUES ('801', 'HVAC_CompressorComsumpPwr', '17', '10', '0', '10', '0', '0', '8000', 'w', 'BCM');
+INSERT INTO `cansignal` VALUES ('801', 'HVAC_CorrectedCabinTemp', '15', '8', '0', '0', '-40', '-40', '87', '??C', 'CDU');
+INSERT INTO `cansignal` VALUES ('801', 'HVAC_CorrectedCabinTempVD', '18', '1', '0', '1', '0', '0', '1', '', 'CDU');
+INSERT INTO `cansignal` VALUES ('801', 'HVAC_PTCPwrAct', '33', '10', '0', '10', '0', '0', '8000', 'w', 'BCM');
+INSERT INTO `cansignal` VALUES ('801', 'HVAC_RawCabinTemp', '7', '8', '0', '0', '-40', '-40', '87', '??C', 'CDU');
+INSERT INTO `cansignal` VALUES ('801', 'HVAC_RawCabinTempVD', '19', '1', '0', '1', '0', '0', '1', '', 'CDU');
+INSERT INTO `cansignal` VALUES ('801', 'HVAC_stPTCAct', '55', '3', '0', '1', '0', '0', '1', '', 'BCM');
+INSERT INTO `cansignal` VALUES ('837', 'ESC_VehSpd', '36', '13', '0', '0', '0', '0', '240', '', 'BCM,PEPS,ICM,AVM,CDU');
+INSERT INTO `cansignal` VALUES ('837', 'ESC_VehSpdVD', '37', '1', '0', '1', '0', '0', '1', '', 'BCM,PEPS,ICM,AVM,CDU');
+INSERT INTO `cansignal` VALUES ('856', 'CDU_ControlSt', '55', '1', '0', '1', '0', '0', '1', '', 'HVAC');
+INSERT INTO `cansignal` VALUES ('856', 'CDU_HVACACButtonSt', '20', '1', '0', '1', '0', '0', '1', '', 'HVAC');
+INSERT INTO `cansignal` VALUES ('856', 'CDU_HVACACButtonStVD', '21', '1', '0', '1', '0', '0', '1', '', 'HVAC');
+INSERT INTO `cansignal` VALUES ('856', 'CDU_HVACACMaxButtonSt', '22', '1', '0', '1', '0', '0', '1', '', 'HVAC');
+INSERT INTO `cansignal` VALUES ('856', 'CDU_HVACACMaxButtonStVD', '23', '1', '0', '1', '0', '0', '1', '', 'HVAC');
+INSERT INTO `cansignal` VALUES ('856', 'CDU_HVACAutoModeButtonSt', '2', '1', '0', '1', '0', '0', '1', '', 'HVAC');
+INSERT INTO `cansignal` VALUES ('856', 'CDU_HVACAutoModeButtonStVD', '3', '1', '0', '1', '0', '0', '1', '', 'HVAC');
+INSERT INTO `cansignal` VALUES ('856', 'CDU_HVACCirculationButtonSt', '18', '1', '0', '1', '0', '0', '1', '', 'HVAC');
+INSERT INTO `cansignal` VALUES ('856', 'CDU_HVACCirculationButtonStVD', '19', '1', '0', '1', '0', '0', '1', '', 'HVAC');
+INSERT INTO `cansignal` VALUES ('856', 'CDU_HVACCtrlModeSt', '54', '3', '0', '1', '0', '0', '7', '', 'HVAC');
+INSERT INTO `cansignal` VALUES ('856', 'CDU_HVACDualButtonSt', '10', '1', '0', '1', '0', '0', '1', '', 'HVAC');
+INSERT INTO `cansignal` VALUES ('856', 'CDU_HVACDualButtonStVD', '11', '1', '0', '1', '0', '0', '1', '', 'HVAC');
+INSERT INTO `cansignal` VALUES ('856', 'CDU_HVACFDefrostButtonSt', '6', '1', '0', '1', '0', '0', '1', '', 'HVAC');
+INSERT INTO `cansignal` VALUES ('856', 'CDU_HVACFDefrostButtonStVD', '7', '1', '0', '1', '0', '0', '1', '', 'HVAC');
+INSERT INTO `cansignal` VALUES ('856', 'CDU_HVACIonButtonSt', '12', '1', '0', '1', '0', '0', '1', '', 'HVAC');
+INSERT INTO `cansignal` VALUES ('856', 'CDU_HVACIonButtonStVD', '13', '1', '0', '1', '0', '0', '1', '', 'HVAC');
+INSERT INTO `cansignal` VALUES ('856', 'CDU_HVACModeButtonSt', '26', '3', '0', '1', '0', '0', '7', '', 'HVAC');
+INSERT INTO `cansignal` VALUES ('856', 'CDU_HVACOffButtonSt', '0', '1', '0', '1', '0', '0', '1', '', 'HVAC');
+INSERT INTO `cansignal` VALUES ('856', 'CDU_HVACOffButtonStVD', '1', '1', '0', '1', '0', '0', '1', '', 'HVAC');
+INSERT INTO `cansignal` VALUES ('856', 'CDU_HVAC_DriverTempSelect', '36', '5', '0', '0', '18', '18', '32', '??C', ' Vector__XXX');
+INSERT INTO `cansignal` VALUES ('856', 'HVAC_PsnTempSelect', '44', '5', '0', '0', '18', '18', '32', '', ' Vector__XXX');
+INSERT INTO `cansignal` VALUES ('856', 'HVAC_WindExitSpd', '30', '4', '0', '1', '0', '0', '15', '', ' Vector__XXX');
+INSERT INTO `cansignal` VALUES ('864', 'HVAC_ACPCommand', '2', '2', '0', '1', '0', '0', '3', '', 'ACP');
+INSERT INTO `cansignal` VALUES ('864', 'HVAC_ACPCommandVD', '0', '1', '0', '1', '0', '0', '1', '', 'ACP');
+INSERT INTO `cansignal` VALUES ('864', 'HVAC_ACPHighSidePress', '21', '6', '0', '0', '0', '0', '31', '', 'ACP');
+INSERT INTO `cansignal` VALUES ('864', 'HVAC_ACPSpeedSet', '14', '7', '0', '100', '0', '0', '8600', '', 'ACP');
+INSERT INTO `cansignal` VALUES ('864', 'HVAC_Checksum', '39', '8', '0', '1', '0', '155', '255', '', 'PTC');
+INSERT INTO `cansignal` VALUES ('864', 'HVAC_PTCPowerRatio', '31', '8', '0', '1', '0', '0', '100', '', 'PTC');
+INSERT INTO `cansignal` VALUES ('867', 'ACPComsumpPwr', '15', '10', '0', '10', '0', '0', '8000', '', 'HVAC');
+INSERT INTO `cansignal` VALUES ('867', 'ACP_BaseState', '63', '3', '0', '1', '0', '0', '7', '', 'HVAC');
+INSERT INTO `cansignal` VALUES ('867', 'ACP_Current', '16', '9', '0', '0', '0', '0', '51', '', 'HVAC');
+INSERT INTO `cansignal` VALUES ('867', 'ACP_ExtState', '58', '3', '0', '1', '0', '0', '7', '', 'HVAC');
+INSERT INTO `cansignal` VALUES ('867', 'ACP_FailGrade', '60', '2', '0', '1', '0', '0', '3', '', 'HVAC');
+INSERT INTO `cansignal` VALUES ('867', 'ACP_HearBeat', '55', '4', '0', '1', '0', '0', '15', '', 'HVAC');
+INSERT INTO `cansignal` VALUES ('867', 'ACP_MotorTemp', '39', '8', '0', '1', '-40', '-40', '140', '', 'HVAC');
+INSERT INTO `cansignal` VALUES ('867', 'ACP_Speed', '6', '7', '0', '100', '0', '0', '8600', '', 'HVAC');
+INSERT INTO `cansignal` VALUES ('868', 'PTCActst', '26', '3', '0', '1', '0', '0', '7', '', 'HVAC');
+INSERT INTO `cansignal` VALUES ('868', 'PTCPwrAct', '23', '10', '0', '10', '0', '0', '8000', 'w', 'HVAC');
+INSERT INTO `cansignal` VALUES ('868', 'PTC_Current', '15', '8', '0', '0', '0', '0', '25', 'A', 'HVAC');
+INSERT INTO `cansignal` VALUES ('868', 'PTC_ElementError', '7', '4', '0', '1', '0', '0', '15', '', 'HVAC');
+INSERT INTO `cansignal` VALUES ('868', 'PTC_InternalError', '1', '2', '0', '1', '0', '0', '3', '', 'HVAC');
+INSERT INTO `cansignal` VALUES ('868', 'PTC_TemperatureOver', '3', '1', '0', '1', '0', '0', '1', '', 'HVAC');
+INSERT INTO `cansignal` VALUES ('868', 'PTC_VoltageFault', '2', '1', '0', '1', '0', '0', '1', '', 'HVAC');
+INSERT INTO `cansignal` VALUES ('915', 'VCU_AirCompressorReq', '36', '1', '0', '1', '0', '0', '1', '', 'HVAC');
+INSERT INTO `cansignal` VALUES ('915', 'VCU_AirCompressorReqVD', '37', '1', '0', '1', '0', '0', '1', '', 'HVAC');
+INSERT INTO `cansignal` VALUES ('915', 'VCU_CompressorPwrLimit', '21', '6', '0', '100', '0', '0', '6000', 'w', 'HVAC');
+INSERT INTO `cansignal` VALUES ('915', 'VCU_CompressorPwrLimitAct', '32', '1', '0', '1', '0', '0', '1', '', 'HVAC');
+INSERT INTO `cansignal` VALUES ('915', 'VCU_PTCPwrLimit', '29', '6', '0', '100', '0', '0', '6000', 'w', 'HVAC');
+INSERT INTO `cansignal` VALUES ('915', 'VCU_PTCrPwrLimitAct', '33', '1', '0', '1', '0', '0', '1', '', 'HVAC');
+INSERT INTO `cansignal` VALUES ('1056', 'BCM_NMAlive', '8', '1', '0', '1', '0', '0', '1', '', 'BCM,PEPS,ICM,CDU');
+INSERT INTO `cansignal` VALUES ('1056', 'BCM_NMDataField', '31', '40', '0', '1', '0', '0', '1', '', 'BCM,PEPS,ICM,CDU');
+INSERT INTO `cansignal` VALUES ('1056', 'BCM_NMDestAddress', '7', '8', '0', '1', '0', '0', '255', '', 'BCM,PEPS,ICM,CDU');
+INSERT INTO `cansignal` VALUES ('1056', 'BCM_NMLimpHome', '10', '1', '0', '1', '0', '0', '1', '', 'BCM,PEPS,ICM,CDU');
+INSERT INTO `cansignal` VALUES ('1056', 'BCM_NMRing', '9', '1', '0', '1', '0', '0', '1', '', 'BCM,PEPS,ICM,CDU');
+INSERT INTO `cansignal` VALUES ('1056', 'BCM_NMSleepAck', '13', '1', '0', '1', '0', '0', '1', '', 'BCM,PEPS,ICM,CDU');
+INSERT INTO `cansignal` VALUES ('1056', 'BCM_NMSleepInd', '12', '1', '0', '1', '0', '0', '1', '', 'BCM,PEPS,ICM,CDU');
+INSERT INTO `cansignal` VALUES ('1056', 'BCM_NMWakeupOrignin', '23', '8', '0', '1', '0', '0', '255', '', 'BCM,PEPS,ICM,CDU');
+INSERT INTO `cansignal` VALUES ('1067', 'CDU_NMAlive', '8', '1', '0', '1', '0', '0', '1', '', 'BCM,PEPS,ICM,CDU');
+INSERT INTO `cansignal` VALUES ('1067', 'CDU_NMDataField', '31', '40', '0', '1', '0', '0', '1', '', 'BCM,PEPS,ICM,CDU');
+INSERT INTO `cansignal` VALUES ('1067', 'CDU_NMDestAddress', '7', '8', '0', '1', '0', '0', '255', '', 'BCM,PEPS,ICM,CDU');
+INSERT INTO `cansignal` VALUES ('1067', 'CDU_NMLimpHome', '10', '1', '0', '1', '0', '0', '1', '', 'BCM,PEPS,ICM,CDU');
+INSERT INTO `cansignal` VALUES ('1067', 'CDU_NMRing', '9', '1', '0', '1', '0', '0', '1', '', 'BCM,PEPS,ICM,CDU');
+INSERT INTO `cansignal` VALUES ('1067', 'CDU_NMSleepAck', '13', '1', '0', '1', '0', '0', '1', '', 'BCM,PEPS,ICM,CDU');
+INSERT INTO `cansignal` VALUES ('1067', 'CDU_NMSleepInd', '12', '1', '0', '1', '0', '0', '1', '', 'BCM,PEPS,ICM,CDU');
+INSERT INTO `cansignal` VALUES ('1067', 'CDU_NMWakeupOrignin', '23', '8', '0', '1', '0', '0', '255', '', 'BCM,PEPS,ICM,CDU');
---
+++ src/com/ictwsn/dao/currentData/ImportData.java
@@ -0,0 +1,87 @@
+package com.ictwsn.dao.currentData;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
+
+import com.ictwsn.bean.CanMessageBean;
+import com.ictwsn.bean.CanSignalBean;
+import com.ictwsn.util.CurrentConn;
+
+//数据库导入函数
+public class ImportData {     
+	public static void main(String args[]) {
+		String flag = null;
+		Connection conn=CurrentConn.getInstance().getConn(); //获取连接
+		PreparedStatement pstmt=null;
+		
+		String data = "";
+        try {
+        	File file = new File("src/Data.txt");//得到一个指定文件
+            InputStreamReader reader = new InputStreamReader(new FileInputStream(file));//建立一个输入流reader
+            BufferedReader br = new BufferedReader(reader);//建立一个对象，把文件内容转成软件程序能读懂的语句      
+            while((data=br.readLine())!=null) {
+            	System.out.println(data);
+            	Pattern pattern1 = Pattern.compile("(BO).\\s(\\d*)\\s(.*)\\:\\s(\\d)\\s(.*)");
+                Matcher matcher1 = pattern1.matcher(data); 
+                while (matcher1.find()) {
+                	flag=matcher1.group(2);
+                	//System.out.println(matcher1.group(2)+","+matcher1.group(3)+","+matcher1.group(4)+","+matcher1.group(5));
+                	//System.out.println();
+                	try {
+            			String sql="insert into canmessage(id,messageName,dcl,nodeName)values(?,?,?,?)";
+            			pstmt=conn.prepareStatement(sql);  
+            			pstmt.setInt(1, Integer.parseInt(matcher1.group(2)));   //设置参数
+            			pstmt.setString(2, matcher1.group(3));
+            			pstmt.setString(3, matcher1.group(4));
+            			pstmt.setString(4, matcher1.group(5));
+            			pstmt.executeUpdate();
+            		}catch(SQLException e) {
+            			e.printStackTrace();
+            		}
+                }
+                Pattern pattern2 = Pattern.compile("\\s(\\w*)\\s.\\s(\\d*).(\\d*).(.*).\\s.(.*)\\,(.*)\\)\\s\\[(.*)\\|(.*)\\]\\s\"(.*)\"\\s\\s(.*)");
+            	Matcher matcher2=pattern2.matcher(data);
+            	while(matcher2.find()) {
+            		//System.out.print(flag+",");
+            		//System.out.println(matcher2.group(1)+","+matcher2.group(2)+","+matcher2.group(3)+","+matcher2.group(4)+","+matcher2.group(5)+","+matcher2.group(6)+","+matcher2.group(7)+","+matcher2.group(8)+","+matcher2.group(9)+","+matcher2.group(10));
+            		//System.out.println("");
+            		
+            		try {
+            			String sql="insert into cansignal(messageId,signalName,startBit,bitLength,bitType,resolution,offsets,minValue,maxValues,unit,nodeNames)values(?,?,?,?,?,?,?,?,?,?,?)";
+            			pstmt=conn.prepareStatement(sql);
+            			pstmt.setInt(1, Integer.parseInt(flag));
+            			pstmt.setString(2, matcher2.group(1));
+            			pstmt.setInt(3, Integer.parseInt(matcher2.group(2)));
+            			pstmt.setInt(4, Integer.parseInt(matcher2.group(3)));
+            			pstmt.setInt(5, Integer.parseInt(matcher2.group(4)));
+            			pstmt.setDouble(6, Double.parseDouble(matcher2.group(5)));
+            			pstmt.setDouble(7, Double.parseDouble(matcher2.group(6)));
+            			pstmt.setDouble(8, Double.parseDouble(matcher2.group(7)));
+            			pstmt.setDouble(9, Double.parseDouble(matcher2.group(8)));
+            			pstmt.setString(10, matcher2.group(9));
+            			pstmt.setString(11, matcher2.group(10));	
+            			pstmt.executeUpdate();
+            		}catch(SQLException e) {
+            			e.printStackTrace();
+            		}
+            	}
+                
+            }
+            br.close();
+        }catch(Exception e) {
+        	e.printStackTrace();
+        }
+    }
+}
+
