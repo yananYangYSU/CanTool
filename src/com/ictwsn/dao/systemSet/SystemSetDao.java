cdb2d357349a29ce8e58f845f66af97fecb56fa7
yananYangYSU <1131074225@qq.com>
2017-10-24 21:44:25 +0800

    yanan 程序

--- src/com/ictwsn/dao/systemSet/SystemSetDao.java
+++ src/com/ictwsn/dao/systemSet/SystemSetDao.java
@@ -1,9 +1,11 @@
 package com.ictwsn.dao.systemSet;
 
 import java.util.List;
+import com.ictwsn.bean.CanMessageBean;
+import com.ictwsn.bean.CanSignalBean;
  
-
 public interface SystemSetDao {
-	
- 
+	public List<CanMessageBean> getCanMessageList();
+	public List<CanSignalBean> getCanSignalListStr(int id);
+	public int getDclById(int id);
 }
--- src/com/ictwsn/service/systemSet/SystemSetServiceImpl.java
+++ src/com/ictwsn/service/systemSet/SystemSetServiceImpl.java
@@ -1,18 +1,36 @@
 package com.ictwsn.service.systemSet;
 
 import java.util.List;
-
 import javax.annotation.Resource;
-
-import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
-
-
+import com.ictwsn.bean.CanMessageBean;
+import com.ictwsn.bean.CanSignalBean;
 import com.ictwsn.dao.systemSet.SystemSetDao;
 
 @Service
 public class SystemSetServiceImpl implements SystemSetService{
-
 	@Resource SystemSetDao dao;
+
+	@Override
+	public List<CanMessageBean> getCanMessageList() {
+		// TODO Auto-generated method stub
+		return dao.getCanMessageList();
+	}
+
+	@Override
+	public String getCanSignalListStr(int id) {
+		List<CanSignalBean> csbList=dao.getCanSignalListStr(id);
+		StringBuffer csbListStr=new StringBuffer();
+		for(CanSignalBean csb:csbList){
+			csbListStr.append(csb.getSignalName()).append("=0").append("\r\n");
+		}
+		return csbListStr.toString();
+	}
+
+	@Override
+	public int getDclById(int id) {
+		// TODO Auto-generated method stub
+		return dao.getDclById(id);
+	}
 }
 
